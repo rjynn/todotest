daecc6544547ae1a4c6571ed7ff997d013fb543a
Ruby <rjsdejesus@gmail.com>
2014-09-23 22:12:29 -0600

    added test for save and load

--- to_do_list_test/src/com/rjynndee/todolist/test/TodoListManagerTest.java
+++ to_do_list_test/src/com/rjynndee/todolist/test/TodoListManagerTest.java
@@ -1,5 +1,7 @@
 package com.rjynndee.todolist.test;
 
+import java.io.IOException;
+
 import com.rjynndee.todolist.ToDoListManager;
 import com.rjynndee.todolist.TodoList;
 import com.rjynndee.todolist.Todos;
@@ -8,14 +10,24 @@ import android.test.AndroidTestCase;
 
 public class TodoListManagerTest extends AndroidTestCase{
 	public void testToDoListManager(){
-		TodoList tl = new TodoList();
-		Todos todo = new Todos("test");
-		tl.addtodo(todo);
-		ToDoListManager tdlm = new ToDoListManager(getContext());
-		tdlm.saveTodoList(tl);
-		TodoList tl2 = tdlm.loadTodoList();
-		assertTrue("Test in todolist1", tl.contains(todo));
-		assertTrue("Test in todolist2", tl2.contains(todo));
+		try {
+			TodoList tl = new TodoList();
+			Todos todo = new Todos("test");
+			tl.addtodo(todo);
+			ToDoListManager tdlm = new ToDoListManager(getContext());
+			tdlm.saveTodoList(tl);
+			TodoList tl2 = tdlm.loadTodoList();
+			assertTrue("Test not in todolist2 1",tl2.size() == 1);
+			assertTrue("Test not in todolist1", tl.contains(todo));
+			assertTrue("Test not in todolist2", tl2.contains(todo));
+			}
+		catch (IOException e){
+			assertTrue("IOEXCEPTION THROWN"+e.toString(),false);
+		} catch (ClassNotFoundException e) {
+			assertTrue("Class not found"+e.toString(),false);
+			e.printStackTrace();
+		}
+		
 		
 	}
 }
